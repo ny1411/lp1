c6b1196ef7d2fe8097b2672063b4ae1d27417afb
Neeraj Yamaji <56645465+ny1411@users.noreply.github.com>
2024-11-13 23:36:57 +0530

    Update RoundRobin.java

--- spos/RoundRobin.java
+++ spos/RoundRobin.java
@@ -1,121 +1,42 @@
-import java.util.*;
+package java_23;
 
-public class RoundRobin{
+import java.util.Scanner;
+public class Roundfinal1 {
+	 public static void main(String args[]) { 
+         Scanner s = new Scanner(System.in); 
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of processes : ");
-        int n = sc.nextInt();
+       int wtime[],btime[],rtime[],num,quantum,total; 
 
-        System.out.println("Enter quantum time : ");
-        int q = sc.nextInt();
+       wtime = new int[10]; 
+       btime = new int[10]; 
+       rtime = new int[10]; 
 
-        int pid[] = new int[n];
-        int at[] = new int[n];
-        int bt[] = new int[n];
-        int ct[] = new int[n];
-        int tat[] = new int[n];
-        int wt[] = new int[n];
-        int rbt[] = new int[n];
-        int f[] = new int[n];
-
-        float avgwt = 0, avgta = 0;
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter process ID for process " + (i + 1) + ": ");
-            pid[i] = sc.nextInt();
-
-            System.out.println("Enter Arrival Time for process " + (i + 1) + ": ");
-            at[i] = sc.nextInt();
-
-            System.out.println("Enter Burst Time for process " + (i + 1) + ": ");
-            bt[i] = sc.nextInt();
-            rbt[i] = bt[i];
-            f[i] = 0;
-        }
-        System.out.println("Input accepted.");
-
-        // Sort processes based on arrival time
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (at[j] > at[j + 1]) {
-                    int temp;
-
-                    temp = at[j];
-                    at[j] = at[j + 1];
-                    at[j + 1] = temp;
-
-                    temp = bt[j];
-                    bt[j] = bt[j + 1];
-                    bt[j + 1] = temp;
-
-                    temp = pid[j];
-                    pid[j] = pid[j + 1];
-                    pid[j + 1] = temp;
-
-                    temp = rbt[j];
-                    rbt[j] = rbt[j + 1];
-                    rbt[j + 1] = temp;
-                }
-            }
-        }
-
-        // Implementing Round Robin Scheduling
-        int st = 0, tot = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        int i = 0;
-        while (tot < n) {
-            while (i < n && at[i] <= st) {
-                queue.add(i);
-                i++;
-            }
-
-            if (queue.isEmpty()) {
-                st++;
-                continue;
-            }
-
-            int idx = queue.poll();
-
-            if (rbt[idx] > q) {
-                st += q;
-                rbt[idx] -= q;
-            } else {
-                st += rbt[idx];
-                ct[idx] = st;
-                rbt[idx] = 0;
-                tot++;
-            }
-
-            while (i < n && at[i] <= st) {
-                queue.add(i);
-                i++;
-            }
-
-            if (rbt[idx] > 0) {
-                queue.add(idx);
-            }
-        }
-
-        // Calculating turn around time and waiting time
-        for (i = 0; i < n; i++) {
-            tat[i] = ct[i] - at[i];
-            wt[i] = tat[i] - bt[i];
-            avgta += tat[i];
-            avgwt += wt[i];
-        }
-
-        // Printing results
-        System.out.println("\nProcess ID\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time");
-        for (i = 0; i < n; i++) {
-            System.out.println(pid[i] + "\t\t" + at[i] + "\t\t" + bt[i] + "\t\t" + ct[i] + "\t\t\t" + tat[i] + "\t\t\t" + wt[i]);
-        }
-
-        // Printing average turn around time and average waiting time
-        System.out.println("\nAverage Turnaround Time: " + (avgta / n));
-        System.out.println("Average Waiting Time: " + (avgwt / n));
-
-        Scanner s= null;
-		s.close();
-    }
+System.out.print("Enter number of processes(MAX 10): "); 
+num = s.nextInt(); 
+System.out.print("Enter burst time"); 
+for(int i=0;i<num;i++) { System.out.print("\nP["+(i+1)+"]: "); 
+btime[i] = s.nextInt(); rtime[i] = btime[i]; wtime[i]=0; } 
+System.out.print("\n\nEnter quantum: "); quantum = s.nextInt(); 
+int rp = num; int i=0; int time=0; System.out.print("0"); 
+wtime[0]=0; while(rp!=0) { if(rtime[i]>quantum) 
+{ 
+rtime[i]=rtime[i]-quantum; 
+System.out.print(" | P["+(i+1)+"] | "); 
+time+=quantum; 
+System.out.print(time); 
+} 
+else if(rtime[i]<=quantum && rtime[i]>0) 
+{time+=rtime[i]; 
+rtime[i]=rtime[i]-rtime[i]; 
+System.out.print(" | P["+(i+1)+"] | "); 
+rp--; 
+System.out.print(time); 
+} 
+i++; 
+if(i==num) 
+{ 
+i=0; 
+} 
+} 
+} 
 }
